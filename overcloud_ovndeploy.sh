c8f472fca09643b96477af6ce705234a568c6ce0
yogananth subramanian <sy.ananth@gmail.com>
2020-03-24 14:57:31 +0530

    Update overcloud_ovndeploy.sh

--- overcloud_ovndeploy.sh
+++ overcloud_ovndeploy.sh
@@ -7,13 +7,13 @@ fi
 openstack overcloud deploy --templates \
     --timeout 90 \
     -r ~/roles_data.yaml \
-    -n ~/osp13_ovn/network_data.yaml \
+    -n ~/ovn/network_data.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-dpdk.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-sriov.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
-    -e ~/osp13_ovn/debug.yaml \
-    -e ~/osp13_ovn/environment.yaml \
-    -e ~/osp13_ovn/ml2-ovs-dpdk.yaml \
-    -e ~/osp13_ovn/containers-prepare-parameter.yaml
+    -e ~/ovn/debug.yaml \
+    -e ~/ovn/environment.yaml \
+    -e ~/ovn/ml2-ovs-dpdk.yaml \
+    -e ~/ovn/containers-prepare-parameter.yaml
