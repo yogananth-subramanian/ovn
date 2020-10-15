a0ef94e7b01fff81e29846dd173d770e9c9381ab
ysubrama <ysubrama@redhat.com>
2020-10-15 19:48:09 +0530

    update offload-vlan-controllersriov

--- overcloud_deploy_offload.sh
+++ overcloud_deploy_offload.sh
@@ -4,7 +4,7 @@ PARAMS="$*"
 USER_THT="$HOME/osp16_ref"
 
 # Always generate roles_data file
-openstack overcloud roles generate -o $HOME/roles_data.yaml Controller ComputeSriov:ComputeSriovOffload
+openstack overcloud roles generate -o $HOME/roles_data.yaml ControllerSriov ComputeSriov:ComputeSriovOffload
 
 openstack overcloud deploy $PARAMS \
     --templates \
@@ -13,11 +13,10 @@ openstack overcloud deploy $PARAMS \
     -n $USER_THT/network_data.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
-    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovs.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-sriov.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/ovs-hw-offload.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
     -e $USER_THT/environment.yaml \
     -e $USER_THT/network-environment-offload.yaml \
     -e $USER_THT/ml2-ovs-nfv.yaml \
     -e $HOME/containers-prepare-parameter.yaml
-
